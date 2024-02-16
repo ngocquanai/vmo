a519b88873d991983df0e99d0dcacdd1c12ba781
Ngoc Quan <ngocquanofficial@gmail.com>
2024-02-16 13:20:11 +0700

    finish chapter 2, part 1

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707926697';
+const cacheName = 'chirpy-1708064380';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
